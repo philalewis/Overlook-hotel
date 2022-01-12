c477d8a43869d1c803b500b2f19bb3476c74bd1c
Phil Lewis <phillewis1985@gmail.com>
2022-01-12 11:27:34 -0700

    Write user tests

--- test/user-test.js
+++ test/user-test.js
@@ -3,7 +3,7 @@ const expect = chai.expect;
 import User from '../classes/User';
 import users from '../data/sample-customers-data'
 
-describe('See if the tests are running', function() {
+describe('User class', () => {
 
   let wyntonKelley;
   let anonymous;
@@ -22,5 +22,28 @@ describe('See if the tests are running', function() {
     expect(wyntonKelley.id).to.equal(1);
   });
 
-  it('should')
+  it('should have a default id if no user is found', () => {
+    expect(anonymous.id).to.equal(99);
+  });
+
+  it('should have a name', () => {
+    expect(wyntonKelley.name).to.equal('Wynton Kelley');
+  });
+
+  it('should have a guest name', () => {
+    expect(anonymous.name).to.equal('guest');
+  });
+
+  it('should have a username', () => {
+    expect(wyntonKelley.username).to.equal('customer01');
+  });
+
+  it('should have a guest username', () => {
+    expect(anonymous.username).to.equal('guest');
+  });
+
+  it('should have a password', () => {
+    expect(wyntonKelley.password).to.equal('overlook2021');
+    expect(anonymous.password).to.equal('overlook2021');
+  });
 });
